9a9acd040d2f180c22549966964c1e6dba47438e
FrancescoGentile97 <francesco.gentile97@icloud.com>
2023-01-18 17:04:16 +0100

    riscritto il codice e aggiunte nuove classi

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
-require_once "./classes/Prodotto.php";
-require_once "./classes/Categoria.php";
+require_once "./models/Product.php";
+require_once "./models/Category.php";
+require_once "./models/Food.php";
+require_once "./models/Toy.php";
+require_once "./models/Bed.php";
 // creare l'array con i prodotti per poi stamparli con un ciclo sull html con il css preparare la card su bootstrap
 
-$productList = [
-    $newCategory = new Categories("Croccantini", "15,99", "image", "Cibo", "Cane"),
-    $newCategory_2 = new Categories("Croccantini", "15,99", "image", "Cibo", "Cane"),
-    $newCategory_3 = new Categories("Croccantini", "15,99", "image", "Cibo", "Cane"),
-    $newCategory_4 = new Categories("Croccantini", "15,99", "image", "Cibo", "Cane"),
+$catCategory = new Category;
+$dogCategory = new Category;
+
+
+
+$croccantini = new Food ("Croccantini",7.99,$dogCategory,10,"img-placeolder",500);
+$corda = new Toy("Corda per cani", 10.99, $dogCategory, 20, "image-placeolder","Gioco per cani");
+$guinzaglio = new Product("Guinzaglio", 9.99, $dogCategory, 40, "image-placeolder");
+$trasportino = new Product("Trasportino", 20, $catCategory, 30, "image-placeolder");
+
+var_dump($croccantini);
+var_dump($corda);
+var_dump($guinzaglio);
+var_dump($trasportino);
+
+$product = [
+    $croccantini,
+    $corda,
+    $guinzaglio,
+    $trasportino,
+    
 ];
+
 
 ?>
 
@@ -20,8 +40,8 @@ $productList = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
     <title>E-COMMERCE PET</title>
 </head>
@@ -29,21 +49,15 @@ $productList = [
 <body>
 
     <div class="container">
-        <h1>PET SHOP</h1>
+        <h1 class="text-center">PET SHOP</h1>
     </div>
-      <div class="row">
+    <div class="row">
         <div class="col">
             <div class="card">
-                <img src="<?php echo $product->getImage() ?>" class="img-fluid"> 
-                <div class="card-body">
-                    <h4 class=""><?php echo $product->getName()?></h4>
-                    <h5 class="card-text">Category:<?php echo $product->getCategory_name()?></h5>
-                    <h5 class="card-text">Animale:<?php echo $product->type()?></h5>
-                    <h5 class="card-text">Prezzo:<?php echo $product->getPrice()?></h5>
-                </div>
+               
             </div>
         </div>
-      </div>
+    </div>
 </body>
 
 </html>
